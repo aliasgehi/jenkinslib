5e4e81c9f75a05779ecf2fda48febc84bcea4e7b
aliasgehi <126857544+aliasgehi@users.noreply.github.com>
2024-04-18 10:48:52 +0200

    Update Stuff.groovy

--- src/paket/Stuff.groovy
+++ src/paket/Stuff.groovy
@@ -1,7 +1,10 @@
-echo """
-${env.JOB_NAME}
-${env.BUILD_ID}
-${env.NODE_NAME}
-${env.WORKSPACE}
-ALIAS = ${env.ALIAS}
-"""
+def printle() {
+  echo """
+  ${env.JOB_NAME}
+  ${env.BUILD_ID}
+  ${env.NODE_NAME}
+  ${env.WORKSPACE}
+  ALIAS = ${env.ALIAS}
+  """
+}
+
